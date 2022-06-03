7bf14241074b7652fa0736f38c0a305c1557d0f7
Abhishek-Rao-Git <77543486+Abhishek-Rao-Git@users.noreply.github.com>
2022-06-03 10:31:15 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-var ran=Math.floor(Math.random()*6+1);
-var ran2=Math.floor(Math.random()*6+1);
+function roll() {
+	var ran = Math.floor(Math.random() * 6 + 1);
+	var ran2 = Math.floor(Math.random() * 6 + 1);
 
-document.querySelector(".img1").setAttribute("src", "./images/dice"+ran+".png");
-document.querySelector(".img2").setAttribute("src", "./images/dice"+ran2+".png");
+	document.querySelector(".img1").setAttribute("src", "./images/dice" + ran + ".png");
+	document.querySelector(".img2").setAttribute("src", "./images/dice" + ran2 + ".png");
 
 
-if(ran>ran2)
-{
-    document.querySelector("h1").innerHTML="🚩Player 1 Wins!";
-}
-else if(ran2>ran)
-{
-    document.querySelector("h1").innerHTML="Player 2 Wins!🚩";
-}
-else
-{
-    document.querySelector("h1").innerHTML="DRAW!";
+	if (ran > ran2) {
+		document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+	} else if (ran2 > ran) {
+		document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+	} else {
+		document.querySelector("h1").innerHTML = "Draw!";
+	}
 }
